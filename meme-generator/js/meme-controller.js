10a92ab9e54ae2a2f490033ba1d66a355643ebee
xXDaniel123 <64831257+xXDaniel123@users.noreply.github.com>
2020-08-16 14:40:33 +0300

    final upload

--- meme-generator/js/meme-controller.js
+++ meme-generator/js/meme-controller.js
@@ -1,135 +1,318 @@
-'use strict'
+'use strict';
 
 var gCanvas;
 var gCtx;
-var gCurrId;
-var gCurrText;
 var gIsEditing = false;
-
-var gCurrImgObj;
+var gIsMoving = false;
 var gCurrImg;
+var gCurrMovingObject;
+
+// this will be the current edit settings object,
+// to allow easy access to the settings
+
+var gCurrData = {
+    id: null,
+    selectedLineIdx: 0,
+    lines: [{
+        text: DEFAULT_TEXT,
+        size: DEFAULT_TEXT_SIZE,
+        font: 'Impact',
+        align: 'center',
+        color: 'white',
+        pos: {
+            x: null,
+            y: null,
+        }
+    }, ],
+};
 
 function init() {
     createMemes();
     render();
 }
 
-function getUpdateSettings() {
-    gCtx.font = "70px impact"
-    gCtx.fillStyle = "white";
-    gCtx.strokeStyle = "black";
-    gCtx.lineWidth = 4;
-}
-
 function onFinishEdit() {
-    gIsEditing = false;
-    gCurrText = '';
+    // also need to save the session
+    onMouseOut();
+    addToMyImgs(gCurrData);
+    resetCurrData();
     render();
 }
 
 function render() {
-
     var strHTML = ``;
-    var elContainer = document.querySelector('.container');
+    var elContainer = document.querySelector('.imgs-container');
 
     if (!gIsEditing) {
         let imgs = getImgsForDisplay();
         imgs.forEach(function (currObj) {
             strHTML += `<img data-idx="${currObj.id}" 
             class="img-item item-${currObj.id}" src="${currObj.url}" alt="" 
-            onclick="onStartEdit(this)"></img>`
-        })
+            onclick="onStartEdit(this)"></img>`;
+        });
         elContainer.innerHTML = strHTML;
-
     } else if (gIsEditing) {
-
         strHTML += `
-        <div class="canvas-container">
-            <canvas id="canvas" width="700" height="700"></canvas>
-        </div>
-        <div class="controls-container">
-            <input onkeyup="onWrite(this, event)" onblur="onMouseOut()" 
-            class="text-input" type="text" value="">
-            <div class="btn-container">
-                <button class="btn" onclick="onSizeChange(this)"> + </button>
-                <button class="btn" onclick="onSizeChange(this)"> - </button>
-                <button class="btn"> right </button>
-                <button class="btn"> center </button>
-                <button class="btn"> left </button>
-                <button class="btn">delete</button>
+        <div class="editor-container">
+            <div class="canvas-container">
+                <canvas id="canvas" width="550" height="550" onmousemove="onStartMove(event)" onmouseup="onFinishMoving(event)"
+                onmousedown="onMouseDown(event)"></canvas>
             </div>
-        </div>
-        <button onclick="onFinishEdit()">close</button>
-        `
+  
+            <div class="controls-container">
+            <input onkeyup="onWrite(this, event)" onblur="onMouseOut()" class="text-input" type="text" value="">
+                <div class="upper-control-container">
+                    <button class="btn" onclick="onNextLine()"><img class="img-icon" src="icons/change.png" alt=""></button>
+                    <button class="btn" onclick="onAddNewLine()"><img class="img-icon" src="icons/add.png" alt=""></button>
+                    <button class="btn" onclick="onRemoveLine()"><img class="img-icon" src="icons/remove.png" alt=""></button>
+                </div>
+    
+                <div class="mid-control-container">
+
+                <div class="upper-mid-control-container">
+
+                    <button class="btn" data-size="+" onclick="onSizeChange(this)"><img class="img-icon" src="icons/increase-size.png" alt=""></button>
+                    <button class="btn" data-size="-" onclick="onSizeChange(this)"><img class="img-icon" src="icons/decrease-size.png" alt=""></button>
+                    <button class="btn" onclick="onAlignLeft()"><img class="img-icon" src="icons/align-to-left.png" alt=""></button>
+                    <button class="btn" onclick="onAlignCenter()"><img class="img-icon" src="icons/align-center.png" alt=""></button>
+                    <button class="btn" onclick="onAlignRight()"><img class="img-icon" src="icons/align-to-right.png" alt=""></button>
+              
+                </div>
+
+                <div class="low-mid-control-container">
+                        <select class="dropdown" name="" id="">
+                            <option value="impact">Impact</option>
+                            <option value="montserat">Montserat</option>
+                        </select>
+                        <button class="btn"><img class="img-icon" src="icons/a.png" alt=""></button>
+                        <button class="btn"><img class="img-icon" src="icons/color.png" alt=""></button>
+                    </div>
+                </div>
+
+                <div class="lower-control-container">
+                        <div class="asset-container"></div>
+                        <div class="lower-btn-container">
+                            <button class="lower-btn"><a href="#" onclick="onDownload(this)" download="my-img.jpg">Download</a></button>
+                            <button class="lower-btn" onclick="onFinishEdit()">close</button>
+                        </div>
+                </div>
+    
+        </div>       
+         </div>
+        `;
         elContainer.innerHTML = strHTML;
         initImg();
-    }
-}
-
-function initImg() {
-    gCurrImgObj = setImageForDisplay(gCurrId);
-    gCanvas = document.getElementById('canvas');
-    gCtx = gCanvas.getContext('2d');
-
-    gCurrImg = new Image;
-    gCurrImg.onload = function () {
-        gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
-        setInitialText();
-    }
-    gCurrImg.src = 'meme-imgs/' + gCurrImgObj.selectedImgId + '.jpg';
-}
-
-function setInitialText() {
-    getUpdateSettings();
-    // maybe loop through object lines
-    let initialText = gCurrImgObj.lines[0].txt;
-    let textWidth = gCtx.measureText(initialText).width;
-    gCtx.fillText(initialText, gCanvas.width / 2 - (textWidth / 2), 80);
-    gCtx.strokeText(initialText, gCanvas.width / 2 - (textWidth / 2), 80);
-}
-
-function onWrite(text, ev) {
-
-    gCurrText = text.value;
-    let textWidth = gCtx.measureText(gCurrText).width;
-    if (ev.code !== "Backspace") {
-        gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
-        gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
-        gCtx.fillText(gCurrText, gCanvas.width / 2 - (textWidth / 2), 80);
-        gCtx.strokeText(gCurrText, gCanvas.width / 2 - (textWidth / 2), 80);
-    } else if (ev.code === "Backspace") {
-        gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
-        gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
-        gCtx.fillText(gCurrText, gCanvas.width / 2 - (textWidth / 2), 80);
-        gCtx.strokeText(gCurrText, gCanvas.width / 2 - (textWidth / 2), 80);
-    }
-    // updated through the onMouseOut function
-}
-
-function onSizeChange(val) {
-    let increment = val.innerText;
-    let currSettings = gCtx.font
-    let currFontFamily = currSettings.split(" ")[1];
-    let currFontSize = currSettings.split(" ")[0].match(/\d+/g)
-
-
-    if (increment === '+') {
-        // increase size in view
-        // increase size in the service, that in turn will increase size in the model
-    } else if (increment === '-') {
-        // decrease size
-        // decrease size in the service, that in turn will decrease size in the model
-
     }
 }
 
 // onclick
 function onStartEdit(el) {
     gIsEditing = true;
-    gCurrId = +el.dataset.idx;
+    gCurrData.id = +el.dataset.idx;
     render();
 }
 
-function onMouseOut() {
-    updateText(gCurrText);
+function initImg() {
+    gCanvas = document.getElementById('canvas');
+    gCtx = gCanvas.getContext('2d');
+
+    gCurrImg = new Image();
+    gCurrImg.onload = function () {
+        gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
+        setInitialText();
+    };
+    gCurrImg.src = 'meme-imgs/' + gCurrData.id + '.jpg';
 }
+
+function setInitialText() {
+    gCtx.font = DEFAULT_TEXT_SIZE + 'px ' + DEFAULT_FONT;
+    gCtx.fillStyle = DEFAULT_COLOR;
+    gCtx.strokeStyle = 'black';
+    gCtx.lineWidth = 3;
+
+    let textWidth = gCtx.measureText(gCurrData.lines[0].text).width;
+
+    let centerX = gCurrData.lines[gCurrData.selectedLineIdx].pos.x = gCanvas.width / 2 - textWidth / 2;
+    let topY = gCurrData.lines[gCurrData.selectedLineIdx].pos.y = gCanvas.height - (gCanvas.height * 0.85);
+
+    // set the default text
+    gCurrData.lines[gCurrData.selectedLineIdx].font = DEFAULT_FONT;
+    gCurrData.lines[gCurrData.selectedLineIdx].size = DEFAULT_TEXT_SIZE;
+
+    gCtx.fillText(DEFAULT_TEXT, centerX, topY); // draws in the middle
+    gCtx.strokeText(DEFAULT_TEXT, centerX, topY); // draws in the middle
+
+}
+
+function onWrite(text) {
+    gCurrData.lines[gCurrData.selectedLineIdx].text = text.value;
+    drawCanvas();
+}
+
+function onMouseOut() {
+    // sending also pos, default pos is 0
+    updateText(gCurrData.text, 0, gCurrData.size);
+}
+
+function drawCanvas() {
+
+    let currIdx = 0;
+
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+    gCtx.drawImage(gCurrImg, 0, 0, gCanvas.width, gCanvas.height);
+
+    for (let i = 0; i < gCurrData.lines.length; i++) {
+
+        // getting current data for the font
+        gCtx.font = gCurrData.lines[currIdx].size + 'px ' + gCurrData.lines[currIdx].font;
+
+        let currText = gCurrData.lines[currIdx].text;
+        let currY = gCurrData.lines[currIdx].pos.y; // get the y pos
+        let currX = gCurrData.lines[currIdx].pos.x; // get the x pos
+
+        // draws on the canvas the text
+        gCtx.fillText(currText, currX, currY);
+        gCtx.strokeText(currText, currX, currY);
+
+        currIdx++;
+    }
+}
+
+function onSizeChange(val) {
+    if (val.dataset.size === '+')
+        gCurrData.lines[gCurrData.selectedLineIdx].size += 10;
+    else if (
+        val.dataset.size === '-' &&
+        gCurrData.lines[gCurrData.selectedLineIdx].size > 10
+    ) {
+        gCurrData.lines[gCurrData.selectedLineIdx].size -= 10;
+    }
+    drawCanvas();
+}
+
+// mouse down event
+function onMouseDown(ev) {
+    gIsMoving = true;
+
+    let clickX = ev.offsetX;
+    let clickY = ev.offsetY;
+
+    let elClicked = gCurrData.lines.find(function (el) {
+        return clickX > el.pos.x && clickX < (el.pos.x + gCtx.measureText(el.text).width) &&
+            clickY > el.pos.y - el.size && clickY < el.pos.y
+    });
+    gCurrMovingObject = elClicked;
+}
+
+// mouse move event
+function onStartMove(ev) {
+    if (gIsMoving) {
+        gCurrMovingObject.pos.x = ev.offsetX - (gCtx.measureText(gCurrMovingObject.text).width / 2)
+        gCurrMovingObject.pos.y = ev.offsetY + (gCurrMovingObject.size / 2)
+        drawCanvas();
+    }
+}
+
+// mouse up event
+function onFinishMoving() {
+    gCurrMovingObject = null;
+    gIsMoving = false;
+    drawCanvas();
+}
+
+function onAddNewLine() {
+    // only support of 2 lines at the moment
+    if (gCurrData.lines.length >= 2) return;
+
+    let addedLine = {
+        text: DEFAULT_TEXT,
+        size: DEFAULT_TEXT_SIZE,
+        font: DEFAULT_FONT,
+        align: DEFAULT_ALIGN,
+        color: DEFAULT_COLOR,
+        pos: {
+            x: null,
+            y: null,
+        }
+    };
+    gCurrData.lines.push(addedLine);
+
+    addToSelectedIdx(); // updte the selected idx
+    if (gCurrData.lines.length === 0) setInitialText();
+    else checkAddedLine();
+
+    drawCanvas();
+}
+
+function checkAddedLine() {
+    // this will be for the second one added
+    if (gCurrData.lines.length === 2) {
+        let currLine = gCurrData.lines[gCurrData.selectedLineIdx]
+
+        gCtx.font = currLine.size + 'px ' + currLine.font;
+        let textWidth = gCtx.measureText(currLine.text).width;
+
+        currLine.pos.x = gCanvas.width / 2 - textWidth / 2;
+        currLine.pos.y = gCanvas.height - (gCanvas.height * 0.05);
+    }
+}
+
+function addToSelectedIdx() {
+    // only support of 2 lines at the moment
+    // need to fix the bug that after removing the only existing line, it adds to the idx
+    let currLength = gCurrData.lines.length;
+    if (currLength <= 2 && gCurrData.selectedLineIdx === 0) gCurrData.selectedLineIdx++
+}
+
+function subtractSelectedIdx() {
+    // only support of 2 lines at the moment
+    let currLength = gCurrData.lines.length;
+    if (currLength !== 0) gCurrData.selectedLineIdx--
+}
+
+function onRemoveLine() {
+    // TODO: remove selected line
+    gCurrData.lines.splice(gCurrData.selectedLineIdx, 1)
+    subtractSelectedIdx();
+    drawCanvas();
+}
+
+function onNextLine() {
+    // TODO: select the line and add a background around it
+    // when pressing it will update the sleectedLineIdx like the onAddNewLine
+    // when onblur remove all background
+
+}
+
+function onAlignLeft() {
+    let currSelected = gCurrData.selectedLineIdx;
+    gCurrData.lines[currSelected].pos.x = 0;
+    drawCanvas();
+}
+
+function onAlignRight() {
+    let currSelected = gCurrData.selectedLineIdx;
+    let textWidth = gCtx.measureText(gCurrData.lines[currSelected].text).width;
+    gCurrData.lines[currSelected].pos.x = gCanvas.width - textWidth;
+    drawCanvas();
+}
+
+function onAlignCenter() {
+    let currSelected = gCurrData.selectedLineIdx;
+    let textWidth = gCtx.measureText(gCurrData.lines[currSelected].text).width;
+    gCurrData.lines[currSelected].pos.x = gCanvas.width / 2 - textWidth / 2;
+    drawCanvas();
+}
+
+function onDownload(el) {
+    var imgContent = gCanvas.toDataURL('image/jpeg');
+    el.href = imgContent;
+
+}
+
+// -------
+
+// unfortunately I had done some major mistakes at the beggining of the project,
+// and only discovered themlater on when it started to take its toll, 
+// I had to go back and rewrite a big chunk of the code and it took me a long time.. 
+// but I learned my lesson well ;]
--- meme-generator/js/meme-service.js
+++ meme-generator/js/meme-service.js
@@ -1,22 +1,77 @@
 'use strict'
 
-var gNextId = 0;
+const DEFAULT_TEXT_SIZE = 60;
+const DEFAULT_TEXT = 'Your text here';
+const DEFAULT_FONT = 'Impact';
+const DEFAULT_ALIGN = 'center';
+const DEFAULT_COLOR = 'white';
+
 var gImgs = [];
+var gMyImgs = [];
 
 var gMeme = {
     selectedImgId: null,
     selectedLineIdx: null,
 
     lines: [{
-        txt: 'Your text here',
-        size: 70,
-        align: 'center',
-        color: 'black'
+        txt: DEFAULT_TEXT,
+        size: DEFAULT_TEXT_SIZE,
+        font: DEFAULT_FONT,
+        align: DEFAULT_ALIGN,
+        color: DEFAULT_COLOR
     }]
 }
 
+function updateTextSize(updatedSize, pos = 0) {
+    gMeme.lines[pos].size = updatedSize;
+}
+
+function addToMyImgs(gCurrData) {
+
+    let objToPush = {}
+    for (let property in gCurrData) {
+        objToPush[property] = gCurrData[property]
+    }
+    gMyImgs.push(objToPush);
+}
+
+function resetCurrData() {
+    gIsEditing = false;
+    gCurrData.lines[0].text = '';
+    gCurrData.selectedLineIdx = 0;
+    gCurrData.lines = [];
+    gCurrData.lines = [{
+        text: DEFAULT_TEXT,
+        size: DEFAULT_TEXT_SIZE,
+        font: 'Impact',
+        align: 'center',
+        pos: {
+            x: null,
+            y: null,
+        },
+        color: 'white',
+    }, ];
+}
+
+function updateText(text, pos = 0, size = DEFAULT_TEXT_SIZE) {
+    gMeme.lines.splice(pos, 1, {
+        txt: text,
+        size: size,
+        font: 'Impact',
+        // align: 'center',
+        // color: 'white'
+    })
+}
+
+function getFontSizeAndFamily() {
+    var str = gMeme.lines[0].size;
+    str += 'px ';
+    str += gMeme.lines[0].font;
+    return str;
+}
+
 function createMemes() {
-    // loop pitfall aheadh, beware!
+    // loop pitfall ahead, beware!
     for (let i = 1; i <= 18; i++) {
         updateModel(i);
     }
@@ -33,26 +88,34 @@ function getImgsForDisplay() {
     return gImgs;
 }
 
-function updateText(text) {
-    gMeme.lines.push({
-        txt: text,
-        size: 70,
-        align: 'center',
-        color: 'black'
-    })
-}
-
 function setImageForDisplay(imgId) {
     gMeme.selectedImgId = imgId;
-    return gMeme;
+    // return gMeme;
 }
 
-function getImgUrl(gCurrId) {
-    var currUrl = null;
-    gImgs.find(function (img) {
-        if (img.id === gCurrId) {
-            currUrl = img.url
-        }
-    })
-    return currUrl;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+// function updateTextSettings(updatedSize, pos = 0) {
+//     gMeme.lines[pos].size = updatedSize;
+// }
+
+
+// function getImgUrl(gCurrId) {
+//     var currUrl = null;
+//     gImgs.find(function (img) {
+//         if (img.id === gCurrId) {
+//             currUrl = img.url
+//         }
+//     })
+//     return currUrl;
+// }
